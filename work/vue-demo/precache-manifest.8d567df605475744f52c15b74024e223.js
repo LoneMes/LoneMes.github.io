3ab5f106afe10f9dfc7d63aab8f8fe8aa6315fca
L <376693576@qq.com>
2019-09-23 15:36:01 +0800

    update test

---
+++ work/vue-demo/precache-manifest.8d567df605475744f52c15b74024e223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/work/vue-demo/robots.txt"
+  },
+  {
+    "revision": "fe437f5bb777bee36d5a",
+    "url": "/work/vue-demo/js/chunk-vendors.6d8651ea.js"
+  },
+  {
+    "revision": "56dd595b0f2b4ed5f7b0",
+    "url": "/work/vue-demo/js/app.08e978fe.js"
+  },
+  {
+    "revision": "539656c96bea2136ac4d",
+    "url": "/work/vue-demo/js/about.f22333b8.js"
+  },
+  {
+    "revision": "46fa1a0cf4a183a900e46f0478ffb94a",
+    "url": "/work/vue-demo/index.html"
+  },
+  {
+    "revision": "56dd595b0f2b4ed5f7b0",
+    "url": "/work/vue-demo/css/app.710a067b.css"
+  }
+];
