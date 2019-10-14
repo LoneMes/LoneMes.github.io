5a21e64e5300fedc6ea5ae31be0ef241af71684f
L <376693576@qq.com>
2019-10-14 19:07:42 +0800

    update pwa

--- work/pwa/dist/notfound.188600c8.chunk.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{524:function(n,e){},529:function(n,e,t){"use strict";t.r(e);var o=t(0),a=t.n(o),c=t(9);t(524);e.default=Object(c.b)(function(n){return{}},function(n){return{actions:{}}})(function(){return a.a.createElement("div",{className:"page-notfound"},a.a.createElement("div",{className:"box"},"404 not found"))})}}]);
---
+++ work/pwa/dist/notfound.33abba2f.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{695:function(n,e,t){"use strict";t.r(e);var o=t(0),a=t.n(o),c=t(96);t(915);e.default=Object(c.b)((function(n){return{}}),(function(n){return{actions:{}}}))((function(){return a.a.createElement("div",{className:"page-notfound"},a.a.createElement("div",{className:"box"},"404 not found"))}))},915:function(n,e,t){}}]);
