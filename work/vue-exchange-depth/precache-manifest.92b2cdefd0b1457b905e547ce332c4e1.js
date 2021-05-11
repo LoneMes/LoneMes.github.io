7b62c805419f93f1f99c6cf4a3d23ad34701c188
javaLuo <376693576@qq.com>
2021-05-11 14:03:13 +0800

    update

---
+++ work/vue-exchange-depth/precache-manifest.92b2cdefd0b1457b905e547ce332c4e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a236f38d8b80cea70e68",
+    "url": "/work/vue-exchange-depth/css/app.82da591d.css"
+  },
+  {
+    "revision": "d9f30dadbd73394ed8ef",
+    "url": "/work/vue-exchange-depth/css/chunk-vendors.4739ea3f.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/work/vue-exchange-depth/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/work/vue-exchange-depth/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "1f57299b15063feebc1bf7a996158b61",
+    "url": "/work/vue-exchange-depth/index.html"
+  },
+  {
+    "revision": "a236f38d8b80cea70e68",
+    "url": "/work/vue-exchange-depth/js/app.dd47fe94.js"
+  },
+  {
+    "revision": "d9f30dadbd73394ed8ef",
+    "url": "/work/vue-exchange-depth/js/chunk-vendors.35eb91f6.js"
+  },
+  {
+    "revision": "608704773ac9318d795191d9cadf8189",
+    "url": "/work/vue-exchange-depth/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/work/vue-exchange-depth/robots.txt"
+  }
+]);
