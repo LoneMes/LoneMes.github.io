d1d61a2532440c5904c6a26703b65a96df900696
javaLuo <376693576@qq.com>
2022-03-17 23:26:56 +0800

    update

--- work/water/libs/index.js
+++ work/water/libs/index.js
@@ -55,6 +55,7 @@ const users = [
   { n: "Rose Dry Leaf Gas", s: 10, w: "我们是同志了" },
   { n: "邶陂以北", s: 0.1, w: "真就这些，不多，世界属于三体，跃迁中" },
   { n: "王", s: 2 },
+  { n: "23:33", s: 1, w: "太酷了" },
 ];
 let loadingCount = 3; // 总共有多少资源需要加载
 let loadingPercent = 0; // 当前加载进度
