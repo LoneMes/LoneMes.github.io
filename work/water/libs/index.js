440b5dc46fe5d3869141ee0e76ed1b46b770657e
javaLuo <376693576@qq.com>
2023-01-17 13:21:09 +0800

    update

--- work/water/libs/index.js
+++ work/water/libs/index.js
@@ -63,6 +63,7 @@ const users = [
   { n: "care", s: 1, w: "鑫哥，你真棒" },
   { n: "朱昀栎", s: 10 },
   { n: "佚名", s: 10.01 },
+  { n: "滴滴香小单", s: 100, w: "水滴计划由蓝色空间号独家赞助" },
 ];
 let loadingCount = 3; // 总共有多少资源需要加载
 let loadingPercent = 0; // 当前加载进度
